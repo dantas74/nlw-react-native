21760211ab518bfb79655c84979d0fcf4302fe47
matheus-dr <m47heusdr@protonmail.com>
2022-07-24 20:08:39 -0300

    feat: integration with firebase

--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
-import { NativeBaseProvider, StatusBar } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {NativeBaseProvider, StatusBar} from 'native-base';
+import {Roboto_400Regular, Roboto_700Bold, useFonts} from '@expo-google-fonts/roboto';
 
-import { THEME } from './src/styles/theme';
+import {THEME} from './src/styles/theme';
 
-import { Loading } from './src/components/Loading';
 import {Routes} from "./src/routes";
+import {Loading} from './src/components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+    const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </NativeBaseProvider>
-  );
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            {fontsLoaded ? <Routes/> : <Loading/>}
+        </NativeBaseProvider>
+    );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,93 +1,132 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import auth from '@react-native-firebase/auth';
+import {useNavigation} from "@react-navigation/native";
+import firestore from "@react-native-firebase/firestore";
 import {ChatTeardropText, SignOut} from "phosphor-react-native";
 import {Center, Heading, HStack, IconButton, Text, useTheme, VStack} from "native-base";
 
 import Logo from "../assets/logo_secondary.svg";
+
 import {Filter} from "../components/Filter";
-import {FlatList} from "react-native";
-import {Order, OrderProps} from "../components/Order";
 import {Button} from "../components/Button";
-import {useNavigation} from "@react-navigation/native";
+import {Alert, FlatList} from "react-native";
+import {Loading} from "../components/Loading";
+import {Order, OrderProps} from "../components/Order";
+import {dateFormat} from "../utils/firestoreDateFormat";
 
 export function Home() {
-  const [statusSelected, setStatusSelected] = useState<"open" | "closed">("open")
-  const [orders, setOrders] = useState<OrderProps[]>([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [statusSelected, setStatusSelected] = useState<"open" | "closed">("open");
+    const [orders, setOrders] = useState<OrderProps[]>([]);
 
-  const { colors } = useTheme()
-  const navigation = useNavigation()
+    const {colors} = useTheme();
+    const navigation = useNavigation();
 
-  function handleNewOrder() {
-    navigation.navigate('new')
-  }
+    function handleNewOrder() {
+        navigation.navigate('new');
+    }
 
-  function handleOpenDetails(orderId: string) {
-    navigation.navigate('details', { orderId })
-  }
+    function handleOpenDetails(orderId: string) {
+        navigation.navigate('details', {orderId});
+    }
 
-  return (
-    <VStack flex={1} pb={6} bg={"gray.700"}>
-      <HStack
-        w={"full"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        bg={"gray.600"}
-        pt={12}
-        pb={5}
-        px={6}
-      >
-        <Logo />
+    function handleLogout() {
+        auth()
+            .signOut()
+            .catch(error => {
+                console.log(error);
+                return Alert.alert('Sair', 'Não foi possível sair.');
+            });
+    }
 
-        <IconButton
-          icon={<SignOut size={26} color={colors.gray[300]} />}
-        />
-      </HStack>
+    useEffect(() => {
+        setIsLoading(true);
 
-      <VStack flex={1} px={6}>
-        <HStack w={"full"} mt={8} mb={4} justifyContent={"space-between"}>
-          <Heading>
-            Meus chamados
-          </Heading>
+        firestore()
+            .collection('orders')
+            .where('status', '==', statusSelected)
+            .onSnapshot(snapshot => {
+                const data = snapshot.docs.map(doc => {
+                    const {patrimony, description, status, created_at} = doc.data();
 
-          <Text color={"gray.200"}>
-            74
-          </Text>
-        </HStack>
+                    return {
+                        id: doc.id,
+                        patrimony,
+                        description,
+                        status,
+                        when: dateFormat(created_at)
+                    }
+                });
 
-        <HStack space={3} mb={8}>
-          <Filter
-            title={"Em andamento"}
-            type={"open"}
-            onPress={() => setStatusSelected("open")}
-            isActive={statusSelected === "open"}
-          />
+                setOrders(data);
+                setIsLoading(false);
+            });
+    }, [statusSelected]);
 
-          <Filter
-            title={"Finalizados"}
-            type={"closed"}
-            onPress={() => setStatusSelected("closed")}
-            isActive={statusSelected === "closed"}
-          />
-        </HStack>
+    return (
+        <VStack flex={1} pb={6} bg="gray.700">
+            <HStack
+                w="full"
+                justifyContent="space-between"
+                alignItems="center"
+                bg="gray.600"
+                pt={12}
+                pb={5}
+                px={6}
+            >
+                <Logo/>
 
-        <FlatList
-          data={orders}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-          ListEmptyComponent={() => (
-            <Center>
-              <ChatTeardropText color={colors.gray[300]} size={40} />
-              <Text color={"gray.300"} fontSize={"xl"} mt={6} textAlign={"center"}>
-                Você ainda não possui {'\n'}
-                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-              </Text>
-            </Center>
-          )}
-        />
+                <IconButton
+                    icon={<SignOut size={26} color={colors.gray[300]}/>}
+                    onPress={handleLogout}
+                />
+            </HStack>
 
-        <Button title={"Nova Solicitação"} onPress={handleNewOrder} />
-      </VStack>
-    </VStack>
-  );
+            <VStack flex={1} px={6}>
+                <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
+                    <Heading color="gray.100">
+                        Solicitações
+                    </Heading>
+                    <Text color="gray.200">
+                        {orders.length}
+                    </Text>
+                </HStack>
+                <HStack space={3} mb={8}>
+                    <Filter
+                        type="open"
+                        title="em andamento"
+                        onPress={() => setStatusSelected('open')}
+                        isActive={statusSelected === 'open'}
+                    />
+                    <Filter
+                        type="closed"
+                        title="finalizados"
+                        onPress={() => setStatusSelected('closed')}
+                        isActive={statusSelected === 'closed'}
+                    />
+                </HStack>
+                {
+                    isLoading ? <Loading/> :
+                        <FlatList
+                            data={orders}
+                            keyExtractor={item => item.id}
+                            renderItem={({item}) => <Order data={item} onPress={() => handleOpenDetails(item.id)}/>}
+                            showsVerticalScrollIndicator={false}
+                            contentContainerStyle={{paddingBottom: 100}}
+                            ListEmptyComponent={() => (
+                                <Center>
+                                    <ChatTeardropText color={colors.gray[300]} size={40}/>
+                                    <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                        Você ainda não possui {'\n'}
+                                        solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                                    </Text>
+                                </Center>
+                            )}
+                        />
+                }
+
+                <Button title="Nova solicitação" onPress={handleNewOrder}/>
+            </VStack>
+        </VStack>
+    );
 }
